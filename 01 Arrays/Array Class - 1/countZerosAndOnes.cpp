6da0d96017b4eca2f65f4774bcf5c9b2d5f9293f
Abhishek kumar <abhishektbg5803@gmail.com>
2025-01-25 15:01:47 +0530

    Update countZerosAndOnes.cpp

--- 01 Arrays/Array Class - 1/countZerosAndOnes.cpp
+++ 01 Arrays/Array Class - 1/countZerosAndOnes.cpp
@@ -1,25 +1,21 @@
 #include<iostream>
 #include<limits.h>
 using namespace std;
-void CoutZeroAndOne(int arr[],int size){
-    int zero = 0;
-    int one = 0;
+bool TargetElement(int arr[],int size){
+    int targetelement=89;
+    for(int i=0;i<size;i++){
+        if(arr[i]==targetelement){
+            return true;
+        }
     
-    for(int i=0; i<size; i++){
-        if(arr[i]==0){
-        zero++;}
-        if(arr[i]==1){
-        one++;}
     }
-    cout<<"Zero"<<":"<<zero<<" ";
-    cout<<"one"<<":"<<one<<":";
-    
+    return  false;
     
 }
 int main(){
-   int arr[]={1,0,0,1,0,1,0};
+   int arr[]={12,13,15,16,19,18,79};
    int size = 7;
- CoutZeroAndOne(arr,size);
+ cout<<TargetElement(arr,size);
     
     
     return 0;
