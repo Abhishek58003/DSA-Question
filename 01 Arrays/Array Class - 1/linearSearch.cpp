5d330abe5fd4142321f29545dbafbb7b050aaf02
Abhishek kumar <abhishektbg5803@gmail.com>
2025-01-25 15:01:25 +0530

    Update linearSearch.cpp

--- 01 Arrays/Array Class - 1/linearSearch.cpp
+++ 01 Arrays/Array Class - 1/linearSearch.cpp
@@ -1,21 +1,25 @@
 #include<iostream>
 #include<limits.h>
 using namespace std;
-bool TargetElement(int arr[],int size){
-    int targetelement=89;
-    for(int i=0;i<size;i++){
-        if(arr[i]==targetelement){
-            return true;
-        }
+void CoutZeroAndOne(int arr[],int size){
+    int zero = 0;
+    int one = 0;
     
+    for(int i=0; i<size; i++){
+        if(arr[i]==0){
+        zero++;}
+        if(arr[i]==1){
+        one++;}
     }
-    return  false;
+    cout<<"Zero"<<":"<<zero<<" ";
+    cout<<"one"<<":"<<one<<":";
+    
     
 }
 int main(){
-   int arr[]={12,13,15,16,19,18,79};
+   int arr[]={1,0,0,1,0,1,0};
    int size = 7;
- cout<<TargetElement(arr,size);
+ CoutZeroAndOne(arr,size);
     
     
     return 0;
