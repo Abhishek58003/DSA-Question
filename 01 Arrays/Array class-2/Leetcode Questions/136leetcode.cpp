24c6f5fa0a2fd435f3700190057baa5ea345ec93
Abhishek kumar <abhishektbg5803@gmail.com>
2025-01-25 23:39:31 +0530

    Update 136leetcode.cpp

--- 01 Arrays/Array class-2/Leetcode Questions/136leetcode.cpp
+++ 01 Arrays/Array class-2/Leetcode Questions/136leetcode.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+using namespace std;
+int FindUniqueNumber(int arr[],int size){
+    int ans=0;
+    for(int i=0;i<size;i++){
+        ans=ans^arr[i];
+    }
+    return ans;
+}
 
+int main(){
+    int arr[]={1,3,4,1,5,3,4};
+    int size=7;
+    int result = FindUniqueNumber(arr,size);
+    cout<< result<<" ";
+    
+    return 0;
+}
