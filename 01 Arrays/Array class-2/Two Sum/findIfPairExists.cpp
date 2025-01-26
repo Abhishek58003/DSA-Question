ef118f607709e70c6ea7756512418ff0cbe6de39
Abhishek kumar <abhishektbg5803@gmail.com>
2025-01-26 23:11:54 +0530

    Update findIfPairExists.cpp

--- 01 Arrays/Array class-2/Two Sum/findIfPairExists.cpp
+++ 01 Arrays/Array class-2/Two Sum/findIfPairExists.cpp
@@ -1,1 +1,28 @@
+#include<iostream>
+using namespace std;
 
+bool CheckTwoSum(int arr[],int size, int target){
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
+            if(arr[i]+arr[j]==target){
+                return true;
+            }
+            
+        }
+    }
+    return false;
+}
+
+int main(){
+    int arr[]={15,20,30,5,50};
+    int size=5;
+    int target = 31;
+    bool ans = CheckTwoSum(arr,size,target);
+    if(ans==true){
+        cout<<"target found";
+    }
+    else{
+        cout<<"target not found";
+    }
+    return 0;
+}
